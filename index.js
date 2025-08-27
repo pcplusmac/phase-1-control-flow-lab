51ef68ca9db101ba1aabc64754a3a18ea543b731
El Sam <samurai.h@yahoo.com>
2025-08-27 20:04:54 +0800

    all passed

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let distance = feet;
+  let msg;
+
+  if (distance <= 400) {
+    msg = "This one is on me!";
+  }else if(distance <= 2000) {
+    msg = "That will be twenty bucks."
+  }else if(distance <= 2500) {
+    msg = "I will gladly take your thirty bucks."
+  }else if(distance > 2500) {
+    msg = "No can do."
+  } else {
+    msg = "enter the distance."
+  }
+
+  return msg;
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(str){
   // Write your code here!
+  let msg;
+  str === 'NYC' ? (msg = "Ok, sounds good.") : (msg = "No go.")
+  return msg;
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(level){
   // Write your code here!
+  let msg;
+  switch (level) {
+    case "generous":
+      msg = "Thank you so much.";
+      break;
+    case "not as generous":
+      msg = "Thank you.";
+      break;
+    default:
+      msg = "Bye."
+
+
+  }
+  return msg;
 }
+
+console.log(scuberGreetingForFeet(199))
